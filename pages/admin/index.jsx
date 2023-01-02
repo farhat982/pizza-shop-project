63453ff86c477fc77d94e740b58c0d0f5365c80e
farhat982 <112558257+farhat982@users.noreply.github.com>
2023-01-02 01:23:49 +0000

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -121,7 +121,7 @@ const Index = ({ orders, products }) => {
   )
 }
 
-export const getServerSideProps = async (ctx) => {
+export const getStaticProps = async (ctx) => {
   const myCookie = ctx.req?.cookies || ''
 
   if (myCookie.token !== 'SWDw4Cv||663Zp3|zxtp%ok6Ejj') {
